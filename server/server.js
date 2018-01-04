790d4b4a996d600014fe415343a66f5cd06b080c
vladislav.tabala <web.wtm@gmail.com>
2018-01-04 18:27:56 +0200

    WIP: generateMsg util

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+const { generateMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 5000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('new user connected')
+
+    socket.on('disconnect', () => {
+        console.log('user was disconnected')
+    });
+
+    socket.emit('newMessage', generateMessage('Obi', 'Hello'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+    socket.on('createMessage', (message) => {
+        io.emit('newMessage', generateMessage(message.from, message.text))
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server started on port:${port}`)
 });
---
+++ server/utils/message.js
@@ -0,0 +1,9 @@
+const generateMessage = (from, text) => {
+    return {
+        from,
+        text,
+        createAt: new Date().getTime()
+    };
+};
+
+module.exports = { generateMessage }
