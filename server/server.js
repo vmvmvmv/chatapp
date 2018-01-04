be0c7aa05bbe3ee6971ebe7dfb64440c377c8a17
vladislav.tabala <web.wtm@gmail.com>
2018-01-04 16:22:30 +0200

    test

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 5000;
+
 var app = express();
 
 app.use(express.static(publicPath));
